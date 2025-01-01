ce96ae9130846ab3e7d20bf8de182de1fad242bd
devs <fraynilson2003@gmail.com>
2024-12-31 22:15:50 -0500

    init db

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,13 +2,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
+console.log(process.env.DB_URL);
+
 export const dbConfig = TypeOrmModule.forRoot({
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  url: process.env.DB_URL,
   entities: [__dirname + './../modules/**/entities/*.entity{.ts,.js}'],
   synchronize: true,
 
